b05647ddf36059b81b14fcb5b5e6135dcbc1690e
Joxos <xujunhao61@163.com>
2025-10-12 11:56:45 +0800

    update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="ventrun",
-    version="1.0",
+    version="1.1",
     packages=find_packages(),
     entry_points={
         "console_scripts": [
@@ -15,7 +15,7 @@ setup(
     ],
     author="Joxos",
     author_email="xujunhao61@163.com",
-    description="A Python script that bootstraps your application and emits a Main event to ModuVent.",
+    description="Bootstrap your application and emit a Main event to ModuVent.",
     keywords="event-driven main entry moduvent",
     url="https://github.com/Joxos/ventrun",
 )
